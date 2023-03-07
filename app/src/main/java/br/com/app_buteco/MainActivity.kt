de2c52f95552b6ba86ffe82a3bde099c589c8d63
daniel.slino <daniel.slino@grupofleury.com.br>
2023-03-07 12:23:10 -0300

    Tela de cardapio feita, com alguns produtos já incluidos. Classe da Base de produtos e categorias feita - Filtro por categoria feita

--- app/src/main/java/br/com/app_buteco/MainActivity.kt
+++ app/src/main/java/br/com/app_buteco/MainActivity.kt
@@ -2,10 +2,34 @@
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import br.com.app_buteco.databinding.ActivityMainBinding
+import br.com.app_buteco.domain.Produto
+import br.com.app_buteco.fragments.CardapioFragment
 
-class MainActivity : AppCompatActivity() {
+    class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        setFragment(CardapioFragment())
+
     }
+
+    private fun setFragment(fragment: Fragment){
+
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+
+        fragmentTransaction.replace(binding.fragmentsContainer.id, fragment)
+
+        fragmentTransaction.commit()
+
+    }
+
 }
--- app/src/main/java/br/com/app_buteco/fragments/CardapioFragment.kt
+++ app/src/main/java/br/com/app_buteco/fragments/CardapioFragment.kt
@@ -1,60 +1,121 @@
 package br.com.app_buteco.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import br.com.app_buteco.R
+import androidx.recyclerview.selection.SelectionTracker
+import androidx.recyclerview.selection.StorageStrategy
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import br.com.app_buteco.adapter.AdapterCategoria
+import br.com.app_buteco.adapter.AdapterProduto
+import br.com.app_buteco.adapter.tracker.CategoriaKeyProvider
+import br.com.app_buteco.adapter.tracker.CategoriaLockup
+import br.com.app_buteco.adapter.tracker.CategoriaPredicate
+import br.com.app_buteco.databinding.FragmentCardapioBinding
+import br.com.app_buteco.domain.BaseCategorias
+import br.com.app_buteco.domain.BaseProdutos
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [CardapioFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class CardapioFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    private lateinit var binding: FragmentCardapioBinding
+    private lateinit var selectionTracker: SelectionTracker<Long>
+
+    private lateinit var rvProduto: RecyclerView
+    private lateinit var rvCategoria: RecyclerView
+
+    private lateinit var adapterProduto: AdapterProduto
+    private lateinit var adapterCategoria: AdapterCategoria
+
+    private lateinit var baseProdutos: BaseProdutos
+    private lateinit var baseCategorias: BaseCategorias
+
+    companion object {
+
+        const val SELECTION_TACKER_KEY = "selection-tracker-categoria"
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cardapio, container, false)
+    ): View {
+        binding = FragmentCardapioBinding.inflate(inflater)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CardapioFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CardapioFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        baseProdutos = BaseProdutos()
+
+        baseCategorias = BaseCategorias()
+
+        rvProduto = binding.rvCardapio
+        rvCategoria = binding.rvCategorias
+
+        rvProduto.setHasFixedSize(true)
+        rvCategoria.setHasFixedSize(true)
+
+        rvProduto.layoutManager = GridLayoutManager(activity,
+            2, GridLayoutManager.VERTICAL, false)
+
+        rvCategoria.layoutManager = GridLayoutManager(activity, 1,
+            GridLayoutManager.HORIZONTAL, false)
+
+        adapterProduto = AdapterProduto(activity?.baseContext!!)
+        adapterCategoria = AdapterCategoria(activity?.baseContext!!, baseCategorias.getAll()
+        ) {
+
+                id -> setarProdutoByCategoria(id)
+
+        }
+
+        rvProduto.adapter = adapterProduto
+        rvCategoria.adapter = adapterCategoria
+
+        configSelectionTracker(savedInstanceState)
+
+        adapterProduto.setData(baseProdutos.getAll())
+
     }
+
+    private fun configSelectionTracker(savedInstanceState: Bundle?) {
+
+        selectionTracker = SelectionTracker.Builder(
+
+            SELECTION_TACKER_KEY,
+            rvCategoria,
+            CategoriaKeyProvider(baseCategorias.getAll()),
+            CategoriaLockup(rvCategoria),
+            StorageStrategy.createLongStorage()
+
+        ).withSelectionPredicate(CategoriaPredicate()).build()
+
+        (rvCategoria.adapter as AdapterCategoria).selectionTracker = selectionTracker
+
+        if (savedInstanceState != null) {
+
+            selectionTracker.onRestoreInstanceState(savedInstanceState)
+
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        selectionTracker.onSaveInstanceState(outState)
+    }
+
+    fun setarProdutoByCategoria(idCategoria: Int){
+
+        if(idCategoria == -1)
+            adapterProduto.setData(baseProdutos.getAll())
+        else
+            adapterProduto.setData(baseProdutos.getProdutoByCategoria(idCategoria))
+
+    }
+
 }
